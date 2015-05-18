d0fca41e4c94da7a2f35ea3f87ac2fff065ed254
christian winkler <christian.winkler@uni.kn>
2015-05-18 23:07:17 +0200

    Implizite Verfahren, noch nicht ganz Lupenrein und v.a. kein eigenes Lösungsverfahren (Newton oder so)

---
+++ implEuler.m
@@ -0,0 +1,34 @@
+function L = implEuler(R,In)
+% description: DGL Solver mit implizitem Euler Verfahren
+% 
+% input:
+% R ... struct mit Infos zu rechter Seite der DGL
+% R.F Funktion F(t,x) = x'; F: RxR^d -> R^d
+% In ... struct mit
+% In.d Dimension des Systems
+% In.xstart Startwert x0 in R^d
+% In.grid diskr. Zeitgitter in R^(1xm): [t0,t1,...,t_(m-1)]
+%
+% output:
+% L.grid Zeitgitter (Zeitpunkte der berechneten Lösungen)
+% L.x Matrix mit Lösungen x(t_i) in R^(dxm), Jede Spalte: ein x_i, i=0..m-1
+%
+% author: Christian Winkler
+
+m = length(In.grid); %Anzahl der Zeitschritte
+
+L.grid = In.grid;
+L.x = zeros(In.d,m); 
+L.x(:,1) = In.xstart;
+
+%%%%%%%%%%%beginne mit berechnung der lösungen%%%%%%%%%%%%%%%%%%%%%%%
+for it=1:1:m-1
+	t1 = In.grid(it+1);
+	h = In.grid(it+1)-In.grid(it); %breite Zeitschritt
+	x0 = L.x(:,it);
+	Phi = @(x) x0-x+h*R.F(t1,x);
+	% Berechnung der approx. Lösung am nächsten Gitterzeitpunkt mithilfe fsolve (sucht nullstelle von Phi)
+	x1 = fsolve(Phi,x0);
+	%Speichern in der Outputvariablen
+	L.x(:,it+1) = x1;
+end

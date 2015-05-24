3d1ecefbac4c14c48b0d825ac08bba0765366765
christian winkler <christian.winkler@uni.kn>
2015-05-24 11:59:28 +0200

    Solver scheinen zu funktionieren, implizite Mitt.pu sieht komisch aus <F2>

--- num_conv.m
+++ num_conv.m
@@ -5,7 +5,7 @@ a = 0.1;%Parameter in DGL
 y0 = 1;%Startwert
 t0 = 0;
 t1 = 0.5/sqrt(1+a); %Intervall Anfang und Ende
-f = @(t,x) 2*t*(x-a)^2; %rechte Seite
+f = @(t,x) 2*t*(x+a)^2; %rechte Seite
 sol = @(t) 1./(1/(1+a)-t^2) - a;
 %Definition der Butcher Tableaus:
 BT_Ee = [0,0;0,1]; %Euler_expl
@@ -17,8 +17,8 @@ BT_RK = [0,0,0,0,0;0.5,0.5,0,0,0;0.5,0,0.5,0,0;1,0,0,1,0;0,1/6.,1/3.,1/3.,1/6.];
 im = 1;
 printf('\n');
 %  for m=1:50:1000 %Anzahl Zeitschritte
-for i=0:1:3%Anzahl Zeitschritte
-	m = 10^i;
+for i=1:1:9%Anzahl Zeitschritte
+	m = 2^i;
 	printf('\n expl: errAnz. Zeitschritte: %d',m);
 	In = struct('d',1,'xstart',y0,'grid',linspace(t0,t1,m),'BT',zeros(1,1));
 	R.F = f;
@@ -33,8 +33,8 @@ for i=0:1:3%Anzahl Zeitschritte
 	im = im+1;
 end
 im=1;
-for i=0:1:3%Anzahl Zeitschritte
-	m = 10^i;
+for i=1:1:9%Anzahl Zeitschritte
+	m = 2^i;
 	printf('\n impl: Anz. Zeitschritte: %d',m);
 	In = struct('d',1,'xstart',y0,'grid',linspace(t0,t1,m));
 	R.F = f;
@@ -46,34 +46,39 @@ for i=0:1:3%Anzahl Zeitschritte
 	im = im+1;
 end
 %Plot der Lösungen
-%  hold on;
-%  t=linspace(t0,t1,100);
-%  for i=1:length(t)
-%  x(i) = sol(t(i));
-%  end
+hold on;
+t=linspace(t0,t1,100);
+for i=1:length(t)
+x(i) = sol(t(i));
+end
 %  for i=1:5
 %  x_5(i) = sol(L_Ei.grid(i));
 %  end
 %  x_5(i)
-%  plot(t,x);
+plot(t,x);
 %  plot(L_Ei.grid,abs(L_Ei.x-x_5),'r');
 %  plot(L_Mi.grid,abs(L_Mi.x-x_5),'g');
 %  plot(L_Ee.grid,abs(L_Ee.x-x_5),'y');
 %  plot(L_RK.grid,abs(L_RK.x-x_5),'c');
-%  legend('exakte Loesung','impl. Euler','impl. Mitt.','expl. Euler','kl. RK');
-%  xlim([0,0.6]);
-%  ylim([0.9,1.5]);
-%  xlabel('time');
-%  ylabel('y(t)');
-%  print -djpg ddd.jpg
-%  hold off;
+printf('\n Plot: Anz. Zeitschritte: %d',length(L_Ei.grid));
+plot(L_Ei.grid,(L_Ei.x),'r');
+plot(L_Mi.grid,(L_Mi.x),'g');
+plot(L_Ee.grid,(L_Ee.x),'y');
+plot(L_RK.grid,(L_RK.x),'c');
+legend('exakte Loesung','impl. Euler','impl. Mitt.','expl. Euler','kl. RK');
+xlim([0,0.6]);
+ylim([0.9,1.5]);
+xlabel('time');
+ylabel('y(t)');
+print -djpg ddd.jpg
+hold off;
 
 %Bestimme Regressionsgeraden
-lM = log10(M);
-l_Ee=log10(err_Ee);
-l_Ei=log10(err_Ei);
-l_Mi=log10(err_Mi);
-l_RK=log10(err_RK);
+%  lM = log10(M);
+%  l_Ee=log10(err_Ee);
+%  l_Ei=log10(err_Ei);
+%  l_Mi=log10(err_Mi);
+%  l_RK=log10(err_RK);
 %  [r_Ee,m,b] = regression(lM,l_Ee)
 %  [r_Ee,m,b] = regression(lM,l_Ee)
 %  [r_Ee,m,b] = regression(lM,l_Ee)

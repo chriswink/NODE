78f21942952d6fe8db7fb9b2a54fbd016c0563c1
christian winkler <christian.winkler@uni.kn>
2015-05-27 23:31:56 +0200

    alles läuft

--- achterbahn.m
+++ achterbahn.m
@@ -1,7 +1,16 @@
+% description: Achterbahn script für Übungsblatt 2, NODE
+
+% author: Christian Winkler, Alex Blech. 
+% mail: christian.winkler@uni.kn, alexander.blech@uni.kn
+
 PLOT=true;
-bahn = 5;
+FSOLVE = false; %benutze fsolve zur Nullstellenbestimmung oder 
+                %eigene Fixpunktiteration
 m 	= 1;%Masse 1kg
 gr 	= [0;-10];%Erdbeschleunigung
+
+%Frage User nach Bahn
+bahn = input('Wähle Bahn (1-5):');
 
 %Definiere die Bahnen
 if 		bahn==1
@@ -25,14 +34,13 @@ elseif 	bahn==4
 	ddg		= @(x) [0;3.2*pi*sin(8*pi*x)]; %2.Ableitung der Bahn
 	nam		= 'Wellen';
 elseif 	bahn==5
-    
+    %Diese Bahn ist nicht stetig diffbar bei 0, deshalb verschoben um 0.01
+    %nach links
 	g 		= @(x) [0.8*(1.35*pi/2*(x+0.01)-sin(1.35*pi/2*(x+0.01)));...
                     0.8 *(-1+cos((x+0.01)*pi/2)) + 0.8]; %Zykloid
     
     dg		= @(x) [0.8*1.35*pi/2-0.8*1.35*pi/2*cos(1.35*pi/2*(x+0.01));...
                     -sin((x+0.01)*pi/2)*0.8*pi/2]; %1.Ableitung der Bahn
-% 	ddg		= @(x) [0;0]; %2.Ableitung der Bahn
-% 	dg		= @(x) [0.8*1.35*pi/2-0.8*1.35*pi/2*cos(1.35*pi/2*x);-0.8*pi/2*sin(x*pi/2)]; %1.Ableitung der Bahn
 	ddg		= @(x) [0.8*(1.35*pi/2)^2*sin(1.35*pi/2*(x+0.01));...
                     -0.8*(pi/2)^2*cos((x+0.01)*pi/2)]; %2.Ableitung der Bahn
 	nam		= 'Zykloid';
@@ -50,25 +58,31 @@ end
 
 %Definiere 'Rechte Seite' -> AWP vom ÜB auf System 1. Ordnung umgewandelt: y(1):=sigma(t),y(2) = sigma'(t)
 F = @(t,y) [y(2); (1./(norm(dg(y(1))))^2)*(m*gr'*dg(y(1)) - y(2)^2*(ddg(y(1)))'*dg(y(1)))];
-%Definiere Startwert [s(0),y(0)]: s(0) ist der Ort zum Zeitpunkt 0, also Start der Bahn = g(0). y(0): Startgeschw. = 0
+%Definiere Startwert [s(0),y(0)]: s(0) ist der Ort zum Zeitpunkt 0,
+%also Start der Bahn = g(0). y(0): Startgeschw. = 0
 x0 = [0.0;0.0];
 %  x0 = [0;0.5];
 
 %Definiere Intervall zur Lsg und Anzahl Zeitschritte
 t0	=0;
-t1	=1.2;
-m 	=500;
+t1	=1;
+N 	= 500;
+% N= input('Anzahl Zeitschritte:');
 
-%Jetzt alle infos in struct packen für den solver
+%Jetzt alle infos in struct packen für die solver
 BT_RK = [0,0,0,0,0;0.5,0.5,0,0,0;0.5,0,0.5,0,0;1,0,0,1,0;0,1/6.,1/3.,1/3.,1/6.]; % klass. RK Stufe 4
 BT_Ee = [0,0;0,1]; %Euler_expl
-In = struct('d',2,'xstart',x0,'grid',linspace(t0,t1,m),'BT',zeros(0,0));
-In.zerosolver = @fsolve; %Verfahren zur Nullstellenbestimmung
-maxIt = 10000;
-eps = 1.e-4;
-nullit = @(phi,x0) zeroIterate(phi,x0,maxIt,eps);
-In.zerosolver = nullit;
-R.F = F;
+In = struct('d',2,'xstart',x0,'grid',linspace(t0,t1,N),'BT',zeros(0,0));
+if FSOLVE %Verfahren zur Nullstellenbestimmung
+    In.zerosolver = @fsolve; 
+else 
+    maxIt = 10000;
+    eps = 1.e-4;
+    nullit = @(phi,x0) zeroIterate(phi,x0,maxIt,eps);
+    In.zerosolver = nullit;
+end
+
+R.F = F; %Rechte Seite
 
 %Diverse solver anwerfen
 %explizite
@@ -82,15 +96,16 @@ L_Mp = implMipu(R,In);
 %plot height vs. time für verschiedene solver
 figure();
 hold on;
-p	 = plot_time_height(L_RK,nam,'RK expl', g);
-p	 = plot_time_height(L_Ee,nam,'Euler expl', g);
-p	 = plot_time_height(L_Ei,nam,'Euler impl', g);
-p	 = plot_time_height(L_Mp,nam,'impl. Mittelpunkt', g);
-plot(gca(),L_RK.grid,zeros(length(L_RK.grid))); %Einen 'Boden' einzeichnen
-leg1 = sprintf('Bahn: %s, Solver:RK-4',nam);
-leg2 = sprintf('Bahn: %s, Solver:Euler expl.',nam);
-leg3 = sprintf('Bahn: %s, Solver:Euler impl.',nam);
-leg4 = sprintf('Bahn: %s, Solver:impl. Mittelpunkt',nam);
+p	 = plot_time_height(L_RK,nam,'RK expl', g,'-r');
+p	 = plot_time_height(L_Ee,nam,'Euler expl', g, ':r');
+p	 = plot_time_height(L_Ei,nam,'Euler impl', g, '.-g');
+p	 = plot_time_height(L_Mp,nam,'impl. Mittelpunkt', g, '--g');
+plot(gca(),L_RK.grid,zeros(length(L_RK.grid)),':b'); %Einen 'Boden' einzeichnen
+h = (t1-t0)/N;
+leg1 = sprintf('Solver:RK-4, Schrittweite:%.2e',h);
+leg2 = sprintf('Solver:Euler explizit, Schrittweite:%.2e',h);
+leg3 = sprintf('Solver:Euler impl, Schrittweite:%.2e',h);
+leg4 = sprintf('Solver:impl. Mittelpunkt, Schrittweite:%.2e',h);
 
-legend(leg1,leg2,leg3,leg4,'Boden');
+legend(leg1,leg2,leg3,leg4,'Boden','Location','southwest');
 hold off;
--- plot_time_height.m
+++ plot_time_height.m
@@ -1,4 +1,4 @@
-function p = plot_time_height(L,bahn,solver,g)
+function p = plot_time_height(L,bahn,solver,g,linestyle)
 % description: Plot für die Achterbahn Lösungen, Zeit vs. Höhe
 % 
 % input:
@@ -6,19 +6,23 @@ function p = plot_time_height(L,bahn,solver,g)
 % bahn ... string mit Name der Bahn
 % solver ... string mit Name des Solvers
 % g ... funktion der bahn (function handle)
-%
+% linestyle... str mit linestyle specifier,z.B.'--', '.', '.-'
+
 % output:
 % plot
 %
 % author: Christian Winkler, Alex Blech. christian.winkler@uni.kn, alexander.blech@uni.kn
 
 ort=g(L.x(1,:)); %(x,y) Paare: Ort in R^2 zu Gitterzeitpunkten
+h = L.grid(2)-L.grid(1); %Schrittweite
 ax=gca();
-p = plot(ax,L.grid,ort(2,:),'-'); %plotte Zeit auf x-Achse und Höhe auf y-Achse
+p = plot(ax,L.grid,ort(2,:),linestyle); %plotte Zeit auf x-Achse und Höhe auf y-Achse
 xlabel('Time');
 ylabel('Height');
-leg = sprintf('Bahn: %s, Solver:%s',bahn,solver);
+leg = sprintf('Solver:%s Schrittweite:%.2f',solver,h);
 legend(leg);
+titel = sprintf('Bahn: %s',bahn);
+title(titel);
 
 %checken, wo zum ersten Ziellinie überquert wird
 n = find(ort(1,:) >=1, 1);

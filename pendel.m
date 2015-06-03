e9f38ce68a384c313e9147dc87be08875ce18c00
ablech <alexander.blech@uni-konstanz.de>
2015-06-03 13:50:46 +0200

    Revert "test"
    This reverts commit f6470e4a853c88884f6c0c90110cc25f04bf8179.

--- pendel.m
+++ pendel.m
@@ -11,7 +11,7 @@ function pendel()
 %fixpunktiteration in impliziten Verfahren genutzt wird.
 ZEITPLOT = true; PHASENPLOT = true; FSOLVE = false; ANIMATED = true;
 
-g   = 10;%Erdbeschl.
+g   = 9.81;%Erdbeschl.
 l   = 1.0; %Länge des Pendels
 t0  = 0;
 t1  = 30; %intervall, groß, da langzeitverhalten betrachtet wird
